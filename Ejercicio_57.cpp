1f4ad4de9151c0988ca3ada1897d660764a4f71c
Max William <71353270+william9w@users.noreply.github.com>
2020-10-16 19:53:35 -0700

    Update Ejercicio_57.cpp

--- Ejercicio_57.cpp
+++ Ejercicio_57.cpp
@@ -7,7 +7,7 @@ Ejercicio 57: Encontrar el promedio de N numeros. Insertar en primer lugar el va
 
 Autor :Mendoza Perez Max William
 max.mendoza201@tectijuana.edu.mx
-Repositorio:https://github.com/Karen25-2/interfaz.git
+Repositorio:https://github.com/william9w/interfaz.git
 
 */
  #include <cstdlib>
